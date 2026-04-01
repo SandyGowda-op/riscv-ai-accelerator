26196f14393989fa7ff830f2e983d02648ee0c3b
SandyGowda-op <sandygowda659@gmail.com>
2026-04-01 18:42:26 +0530

    Add files via upload

---
+++ code/src/alu.v
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+module alu (
+    input  wire [31:0] a,
+    input  wire [31:0] b,
+    input  wire [3:0]  alu_ctrl,
+    output reg  [31:0] result
+);
+    always @(*) begin
+        case (alu_ctrl)
+            4'b0000: result = a + b;
+            4'b0001: result = a - b;
+            4'b0010: result = a & b;
+            4'b0011: result = a | b;
+            default: result = 32'd0;
+        endcase
+    end
+endmodule
